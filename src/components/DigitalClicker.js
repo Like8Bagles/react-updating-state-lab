a0a710c15ec45b8a0755e010e091178085057985
Anthony <anthonykmbanks@gmail.com>
2021-04-21 09:03:44 -0400

    commit tree f80ea438219630c4ce702de4aec2cc68e697811c

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,19 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+export default class DigitalClicker extends Component {
+    constructor() {
+        super()
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    render() {
+        return(
+            <button onClick={event=> this.setState(state=> ({timesClicked: state.timesClicked + 1}))}>
+                {this.state.timesClicked}
+            </button>
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,36 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react'
+
+export default class YouTubeDebugger extends Component {
+    constructor() {
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <button className="bitrate" onClick={event => this.setState({settings: {...this.state.settings, bitrate: 12}})}>
+                    {this.state.settings.bitrate}
+                </button>
+
+                <button className="resolution"         
+                onClick={event=> {
+                    this.setState(state=> {
+                        state.settings.video.resolution = '720p'
+                        return state})}}>
+                    {this.state.settings.video.resolution}
+                </button>
+            </div>
+        )
+    }
+}
